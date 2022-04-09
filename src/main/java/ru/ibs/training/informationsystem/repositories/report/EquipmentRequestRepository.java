5b03350d1fce44483a61e47300e209b63b38a6ff
RKudrin <RKudrin@ibs.ru>
2022-04-09 16:34:56 +0400

    fix

--- src/main/java/ru/ibs/training/informationsystem/repositories/report/EquipmentRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.ibs.training.informationsystem.repositories.report;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.ibs.training.informationsystem.model.request.EquipmentRequestEntity;
-
-@Repository
-public interface EquipmentRequestRepository extends JpaRepository<EquipmentRequestEntity, Long> {
-}

97a437f9d4ef3f4ed62cfe6e364971dae302956e
RKudrin <RKudrin@ibs.ru>
2022-04-09 17:08:00 +0400

    Ruslan/_feature_1 deleted CodeRepository.java

--- src/main/java/ru/ibs/training/informationsystem/repositories/report/CodeRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.ibs.training.informationsystem.repositories.report;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.ibs.training.informationsystem.model.report.CodeEntity;
-
-@Repository
-public interface CodeRepository extends JpaRepository<CodeEntity, Long> {
-}

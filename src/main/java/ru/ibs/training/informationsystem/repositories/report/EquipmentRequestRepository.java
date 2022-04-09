867d81b03a3123496692d49981456e8b1b89ac77
RKudrin <RKudrin@ibs.ru>
2022-04-09 16:34:56 +0400

    feature_1/ Ruslan made some renames and movements for better merges

--- src/main/java/ru/ibs/training/informationsystem/repositories/report/EquipmentRequestRepository.java
+++ src/main/java/ru/ibs/training/informationsystem/repositories/report/EquipmentRequestRepository.java
@@ -1,4 +1,4 @@
-package ru.ibs.training.informationsystem.repositories.report;
+package ru.ibs.training.informationsystem.repositories.request;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

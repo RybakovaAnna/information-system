0ce5ec8b1427ebc6c277e55122fb29a2056233ea
RKudrin <RKudrin@ibs.ru>
2022-04-09 17:37:18 +0400

    Ruslan/_feature_1 added liquibase migration scripts

---
+++ src/main/resources/db/changelog/1.0/2-create-sequences.sql
@@ -0,0 +1,55 @@
+create sequence application_for_equipment_id_seq
+    increment by 50;
+
+alter sequence application_for_equipment_id_seq owner to postgres;
+
+create sequence commissioned_wells_id_seq
+    increment by 50;
+
+alter sequence commissioned_wells_id_seq owner to postgres;
+
+create sequence executive_id_seq
+    increment by 50;
+
+alter sequence executive_id_seq owner to postgres;
+
+create sequence fund_calendar_time_id_seq
+    increment by 50;
+
+alter sequence fund_calendar_time_id_seq owner to postgres;
+
+create sequence gas_production_id_seq
+    increment by 50;
+
+alter sequence gas_production_id_seq owner to postgres;
+
+create sequence oil_movement_id_seq
+    increment by 50;
+
+alter sequence oil_movement_id_seq owner to postgres;
+
+create sequence oil_preparation_id_seq
+    increment by 50;
+
+alter sequence oil_preparation_id_seq owner to postgres;
+
+create sequence oil_production_id_seq
+    increment by 50;
+
+alter sequence oil_production_id_seq owner to postgres;
+
+create sequence report_id_seq
+    increment by 50;
+
+alter sequence report_id_seq owner to postgres;
+
+create sequence reservoir_oil_production_id_seq
+    increment by 50;
+
+alter sequence reservoir_oil_production_id_seq owner to postgres;
+
+create sequence well_fund_id_seq
+    increment by 50;
+
+alter sequence well_fund_id_seq owner to postgres;
+
